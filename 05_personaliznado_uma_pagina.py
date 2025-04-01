c168355e1de80385b3cbe7c4f841680b3fa2b64d
angel1cad1n1z <angelicadiniz_ad@hotmail.com>
2025-04-01 20:21:21 -0300

    personalizando uma página do app

---
+++ 05_personaliznado_uma_pagina.py
@@ -0,0 +1,46 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.window.always_on_top= True
+    # por padrão todas as alterações do page é importante que sejam feitas no arquivo principal(main)
+    page.bgcolor = "#ff0000"
+        # opções:
+            # "white"
+            # ft.Colors.white (aqui tem as cores já pré-definidas)
+    page.padding = 50 # aplica em  toda a borda o mesmo valor
+    # page.padding = ft.padding.all(100)
+    # page.padding = ft.padding.only(top= 10, left= 20, right= 20, bottom=10)
+    
+    page.spacing = 100 #atribui espaçamento entre os elementos da página
+    
+    page.title = "Primeiros passos no flet" #adiciona um título para a página
+    
+    txt = ft.Text(
+            value= "Olá mundo",
+            bgcolor = "amber",
+            size = 95
+            )
+    txt_container = ft.Container(
+            ft.Text(
+                value = "Bem-Vindo!",
+                bgcolor  = "green",
+                size = 90
+            ),
+            alignment=ft.alignment.top_right
+                # top_left = Alinha no canto superior esquerdo
+                # top_center = Alinha no topo, centralizado
+                # top_right = Alinha no canto superior direito
+                # center_left = Alinha no centro, à esquerda
+                # center = Alinha no centro da tela
+                # center_right = Alinha no centro, à direita
+                # bottom_left = Alinha no canto inferior esquerdo
+                # bottom_center = Alinha no rodapé, centralizado
+                # bottom_right = Alinha no canto inferior direito
+        )
+    
+    page.add(txt, txt_container)
+    
+    page.update()
+    
+    
+ft.app(target= main)
