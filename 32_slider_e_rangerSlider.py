85e63fe641c241e088b49e53b006cc87f79e130e
angel1cad1n1z <angelicadiniz_ad@hotmail.com>
2025-06-25 21:59:54 -0300

    aula_32_slider_e_RangeSlider

--- 32_slider_e_rangerSlider.py
+++ 32_slider_e_rangerSlider.py
@@ -17,7 +17,21 @@ def main(page: ft.Page):
         value=10,
         on_change=lambda _: print("Selecionado", s1.value),
     )
-    page.add(s1)
+    
+    rs1 = ft.RangeSlider(
+        start_value= 20,
+        end_value=90,
+        active_color=ft.Colors.AMBER,
+        inactive_color=ft.Colors.GREY,
+        overlay_color=ft.Colors.AMBER_200,
+        divisions=12, # coloca pontinhos e quando arrastar a bolinha vai de pontinho em pontinho
+        label='{value}',
+        min=0,
+        max=100,
+        round=2, # caso queira um número flutuante usa o round para definir quantas casas decimais serão necessárias - Caso não utilize o sistema arredonda o valor p cima
+        on_change=lambda _: print(rs1.start_value,'->', rs1.start_value),
+    )
+    page.add(s1, rs1)
 
 
 if __name__ == "__main__":
