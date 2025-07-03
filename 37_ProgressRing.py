231ab2052b185401c4697611cbee77413c7fa1ab
angel1cad1n1z <angelicadiniz_ad@hotmail.com>
2025-07-02 21:09:42 -0300

    aula_37_progressRing

--- 37_ProgressRing.py
+++ 37_ProgressRing.py
@@ -1,35 +1,28 @@
 import flet as ft
 import time
 
-# cria barra de progresso
-# estado padrão: rolagem infinita
-
-
 def main(page: ft.Page):
     page.window.always_on_top = True
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-    pg1 = ft.ProgressBar(
-        value=0.3,
-        bar_height=50,
+    pg1 = ft.ProgressRing(
+        value=0.2,
         bgcolor=ft.Colors.GREY_200,
         color=ft.Colors.GREEN,
-        tooltip="Progresso",
+        stroke_width=5,
+        tooltip="Carregando...",
     )
-
-    pg2 = ft.ProgressBar(
-        bar_height=50,
-    )
+    
+    pg2 = ft.ProgressRing()
 
     page.add(pg1, pg2)
-
-    for i in range(10):
+    
+    for i in range(100):
         if pg1.value is not None:
-            pg1.value += 0.1
-            time.sleep(1)  # incrementa de 1 em 1 segundo
+            pg1.value += 0.01
+            time.sleep(0.2)
             page.update(pg1)
-
 
 if __name__ == "__main__":
     ft.app(target=main)
